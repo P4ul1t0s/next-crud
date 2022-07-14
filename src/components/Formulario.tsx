5e784ccb311a1904e6fc182fff36073b60e30a9d
Paulo <paulomafra2004@gmail.com>
2022-07-14 10:23:13 -0300

    Componente Formulário

---
+++ src/components/Formulario.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import Cliente from "../core/Cliente";
+import Botao from "./Botao";
+import Entrada from "./Entrada";
+
+interface FormularioProps{
+    cliente: Cliente
+}
+
+export default function Formulario(props: FormularioProps){
+    const id = props.cliente?.id
+    const [nome, setNome] = useState(props.cliente?.nome ?? '')
+    const [idade, setIdade] = useState(props.cliente?.idade ?? 0)
+
+    return(
+        <div>
+            {id ? (
+                <Entrada somenteLeitura texto="Código" valor={id} className="mb-5"/>
+            ) : (
+                false
+            )}
+            <Entrada texto="Nome" valor={nome} valorMudou={setNome} className="mb-5"/>
+            <Entrada texto="Idade" valor={idade} tipo={"number"} valorMudou={setIdade}/>
+            <div className="flex justify-end mt-7">
+                <Botao cor="blue" classname="mr-4">{id ? 'Alterar' : 'Salvar'}</Botao>
+                <Botao cor="gray">Cancelar</Botao>
+            </div>
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Botao from '../components/Botao'
+import Formulario from '../components/Formulario'
 import Layout from '../components/Layout'
 import Tabela from '../components/Tabela'
 import Cliente from '../core/Cliente'
@@ -26,6 +27,7 @@ export default function Home() {
                     <Botao cor="green" classname="mb-4">Novo Cliente</Botao>
                 </div>
                 <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido}></Tabela>
+                <Formulario cliente={clientes[0]}/>
             </Layout>
         </div>
     )
